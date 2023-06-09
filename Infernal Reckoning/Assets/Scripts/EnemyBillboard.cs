6b9d26631af63657510ff2225dcbabec7ee901d5
JakeClark-AIE <128756368+JakeClark-AIE@users.noreply.github.com>
2023-06-09 15:11:27 +0930

    Built out some of the level

--- Infernal Reckoning/Assets/Scripts/EnemyBillboard.cs
+++ Infernal Reckoning/Assets/Scripts/EnemyBillboard.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnemyBillboard : MonoBehaviour
 {
+    //Stores the direction from the sprite to the camera
     Vector3 cameraDir;
+    //Script for determining the enemy state
     EnemyManagerStateMachine enemyManager;
+    //Assign a 2D sprite for when the enemy is partrolling
     public Sprite patrol;
+    //Assign a 2D sprite for when the enemy is aggro
     public Sprite aggro;
+    //Unity Sprite renderer
     private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Assigning these variables with components on the game object that this script is also attached to
         enemyManager = GetComponent<EnemyManagerStateMachine>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -20,10 +26,14 @@ public class EnemyBillboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get the forward direction of the main camera
         cameraDir = Camera.main.transform.forward;
+        //Set the Y value to zero, ignore verticality 
         cameraDir.y = 0;
 
+        // Rotate the sprite to face the camera
         transform.rotation = Quaternion.LookRotation(cameraDir);
+        //Sets the sprite to either Patrol or Aggro depending on enemy state
         if (enemyManager.enemyState != EnemyManagerStateMachine.EnemyState.patrol)
         {
             spriteRenderer.sprite = aggro;
--- Infernal Reckoning/Assets/Scripts/TakeDamageImage.cs
+++ Infernal Reckoning/Assets/Scripts/TakeDamageImage.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 
 public class TakeDamageImage : MonoBehaviour
 {
+    //Unity UI image of a partially transparent red image
     public Image redScreen;
+    //Sets the duration of the above image appearing on screen
     public float damageEffectDuration = 0.5f;
+    //Timer to keep track of the above damage effect
     private float timer;
 
+    //Checks if the player is currently taking damage
     private bool isTakingDamage;
 
     private void Start()
     {
+        //The game starts with the red screen effect turned off
         redScreen.enabled = false;
     }
 
@@ -21,18 +26,24 @@ public class TakeDamageImage : MonoBehaviour
     {
         if (isTakingDamage)
         {
+            //Counts the time since the last frame
             timer += Time.deltaTime;
+            //Sets the red image on
             redScreen.enabled = true;
+            //Checks if timer has reached the duration
             if (timer > damageEffectDuration)
             {
+
+                //Not taking damage and turns screen effect off
                 isTakingDamage = false;
                 redScreen.enabled = false;
             }
-            
+
         }
     }
     public void TakeDamage()
     {
+        //Resets check for damage and turns screen effect off
         isTakingDamage = true;
         timer = 0;
     }
